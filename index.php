e283c414a5a232309c6dc32547577ffc4debc710
DartVeyder <medvsd.dmitro@gmail.com>
2022-12-06 12:54:34 +0200

    add shifts z-repot

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
+$configs = include_once('class/config.php');
 include_once('class/checkbox.php');
+
+$checkbox = new Checkbox($configs);
 include_once('template/main.html');
-/*
-$checkbox = new Checkbox;
-$response =  $checkbox->getShifts();
+
+
+/*$response =  $checkbox->getShifts();
 print_r($response['results'][0]->status);*/
 
+foreach ($variable as $key => $value) {
+    # code...
+}
